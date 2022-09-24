195eb997ee50ae9142e9d1b973874ee6b9c11939
diegopacheco <diego.pacheco.it@gmail.com>
2022-09-24 14:01:00 -0700

    clojure basics app - ifs

--- clojure-basics/src/clojure_basics/core.clj
+++ clojure-basics/src/clojure_basics/core.clj
@@ -1,7 +1,13 @@
 (ns clojure-basics.core
   (:gen-class))
 
+;; Basic IF in clojure
+(defn cljif [] (if (= 2 2)
+                 "Values are equal"
+                 "Values are not equal"))
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (let [result (cljif)]
+    (println result)))
