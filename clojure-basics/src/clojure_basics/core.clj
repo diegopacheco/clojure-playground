f120350f7d99ba92ee03fc7f2d20abfd43a68e08
diegopacheco <diego.pacheco.it@gmail.com>
2022-09-24 15:08:15 -0700

    .

--- clojure-basics/src/clojure_basics/core.clj
+++ clojure-basics/src/clojure_basics/core.clj
@@ -2,10 +2,6 @@
   (:use [clojure-basics.loops])
   (:gen-class))
 
-;; Generic function to call any function and print the result
-(defn call-n-print [f args] ((let [result (apply f args)]
-                        (println result))))
-
 ;; Basic IF in clojure
 (defn cljif [] (if (= 2 2)
                  "Values are equal"
@@ -14,4 +10,5 @@
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (call-n-print cljif []))
+  (let [result (apply f args)]
+    (println result)))
